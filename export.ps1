e5f7b86b2fecde8b63c7fae7c138a45c5617ae71
Alexandre Liebhaberg <liebhaberg.alexandre@lyceefrancais.be>
2020-03-06 01:11:41 +0100

    Fichier pour envoyer rapidement le code sur GitHub

---
+++ export.ps1
@@ -0,0 +1,17 @@
+﻿if ($args.count -lt 1) {
+    write-host "[ERROR] Usage: ./export.ps1 [commit message]"
+    exit
+}
+$message = $args[0]
+
+Remove-Item -Path ".\_PicAcademy Git\client" -Recurse -Force
+Remove-Item -Path ".\_PicAcademy Git\data" -Recurse -Force
+Remove-Item -Path ".\_PicAcademy Git\server" -Recurse -Force
+cp ".\PicAcademy Client\src\be\alexandreliebh\picacademy\client" ".\_PicAcademy Git\client" -Recurse
+cp ".\PicAcademy Server\src\be\alexandreliebh\picacademy\server" ".\_PicAcademy Git\" -Recurse
+cp ".\PicAcademy Data\src\be\alexandreliebh\picacademy\data" ".\_PicAcademy Git\" -Recurse
+
+cd "_PicAcademy Git"
+git add .
+git commit -m $message
+git push -u origin master
